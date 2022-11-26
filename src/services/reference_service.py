aacb33cec7e7fc857f87ccb422fd7f09c271a9dc
Paul <paul.m.sainio@gmail.com>
2022-11-27 01:59:12 +0200

    book reference submission function

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,3 +1,5 @@
+from repositories.reference_repository import reference_repository as rr
+
 class UserInputError(Exception):
     pass
 
@@ -5,11 +7,20 @@ class ReferenceService():
     def __init__(self):
         pass
 
+    def submit_book_reference(self, book:dict):
+        rr.add_reference(book)
+
     def validate_book_input(self, book:dict):
+        """checks for missing but mandatory info as well as input length
+        """
         if book["author"] == "" and book["editor"] == "":
             raise UserInputError("Author or editor required!")
+        if len(book["author"]) > 100 or len(book["editor"]) > 100:
+            raise UserInputError("Author or editor name too long.")
         if book["title"] == "":
             raise UserInputError("Title required!")
+        if len(book["title"]) > 200:
+            raise UserInputError("Title too long")
         if book["publisher"] == "":
             raise UserInputError("Publisher required!")
         if book["year"] == "":
