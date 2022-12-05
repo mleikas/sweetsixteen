69296d4d2294ae5493a5f57c0db1b973bfe2b92e
Paul <paul.m.sainio@gmail.com>
2022-12-05 14:38:39 +0200

    parser ignores empty values
    co-authored-by: h0gm4n atte.hogman@gmail.com

--- src/services/parser.py
+++ src/services/parser.py
@@ -2,10 +2,18 @@ from repositories.reference_repository import reference_repository as rr
 from bibtexparser import bibdatabase, dump, dumps
 
 
+def filter_empty_values(ref):
+    new_ref = {}
+    for key in ref:
+        if ref[key] != "":
+            new_ref[key] = ref[key]
+
+    return new_ref
+                
 
 def parse():
     database = bibdatabase.BibDatabase()
-    all_references = rr.get_all()
+    all_references = rr.get_all_references_with_entries()
     for reference in all_references:
         del reference["id"]
         reference["ID"] = reference.pop("ref_key")
@@ -14,7 +22,14 @@ def parse():
             reference["ENTRYTYPE"] = "book"
         if reference["ENTRYTYPE"] == 2:
             reference["ENTRYTYPE"] = "article"
+        if reference["ENTRYTYPE"] == 3:
+            reference["ENTRYTYPE"] = "misc"
+        if reference["ENTRYTYPE"] == 4:
+            reference["ENTRYTYPE"] = "phdthesis"
+        if reference["ENTRYTYPE"] == 5:
+            reference["ENTRYTYPE"] = "incollection"
 
+        reference = filter_empty_values(reference)
 
         database.entries.append(reference)
 
