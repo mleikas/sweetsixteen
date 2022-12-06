2468c4558b43145185be4f611fc975602ded6f7f
Paul <paul.m.sainio@gmail.com>
2022-12-06 10:30:56 +0200

    deleted old validation method

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -54,21 +54,4 @@ class ReferenceService():
         if input == "":
             raise UserInputError("Field required!")
 
-    def validate_book_input(self, book: dict):
-        """checks for missing but mandatory info as well as input length
-        """
-        '''if book["author"] == "" and book["editor"] == "":
-            raise UserInputError("Author or editor required!")
-        if len(book["author"]) > 100 or len(book["editor"]) > 100:
-            raise UserInputError("Author or editor name too long.")
-        if book["title"] == "":
-            raise UserInputError("Title required!")
-        if len(book["title"]) > 200:
-            raise UserInputError("Title too long")
-        if book["publisher"] == "":
-            raise UserInputError("Publisher required!")
-        if book["year"] == "":
-            raise UserInputError("Year required!")'''
-        pass
-
 reference_service = ReferenceService()
