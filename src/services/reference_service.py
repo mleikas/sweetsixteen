598089a193a4f812fb86e4cbb9c90f5b52fe21dc
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-12-07 09:54:03 +0200

    Fix few pylint errors

--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,30 +1,33 @@
 from repositories.reference_repository import reference_repository as rr
 
+
 def organise_references_for_output(references):
-        new_references = []
-        for reference in references:
-            del reference["id"]
-            reference["ID"] = reference.pop("ref_key")
-            reference["ENTRYTYPE"] = reference.pop("type_id")
-            if reference["ENTRYTYPE"] == 1:
-                reference["ENTRYTYPE"] = "book"
-            if reference["ENTRYTYPE"] == 2:
-                reference["ENTRYTYPE"] = "article"
-            if reference["ENTRYTYPE"] == 3:
-                reference["ENTRYTYPE"] = "misc"
-            if reference["ENTRYTYPE"] == 4:
-                reference["ENTRYTYPE"] = "phdthesis"
-            if reference["ENTRYTYPE"] == 5:
-                reference["ENTRYTYPE"] = "incollection"
-            new_ref = {}
-            for key in reference:
-                if reference[key] != "":
-                    new_ref[key] = reference[key]
-            new_references.append(new_ref)
-        return new_references
+    new_references = []
+    for reference in references:
+        del reference["id"]
+        reference["ID"] = reference.pop("ref_key")
+        reference["ENTRYTYPE"] = reference.pop("type_id")
+        if reference["ENTRYTYPE"] == 1:
+            reference["ENTRYTYPE"] = "book"
+        if reference["ENTRYTYPE"] == 2:
+            reference["ENTRYTYPE"] = "article"
+        if reference["ENTRYTYPE"] == 3:
+            reference["ENTRYTYPE"] = "misc"
+        if reference["ENTRYTYPE"] == 4:
+            reference["ENTRYTYPE"] = "phdthesis"
+        if reference["ENTRYTYPE"] == 5:
+            reference["ENTRYTYPE"] = "incollection"
+        new_ref = {}
+        for key in reference:
+            if reference[key] != "":
+                new_ref[key] = reference[key]
+        new_references.append(new_ref)
+    return new_references
+
 
 class UserInputError(Exception):
     pass
+
 
 class ReferenceService():
     def __init__(self):
@@ -37,10 +40,10 @@ class ReferenceService():
         return rr.get_reference_entries(ref_id)
 
     def get_fields_by_type_name(self, type_name):
-        references=rr.get_field_types_by_type_name(type_name)
-        new_references={}
+        references = rr.get_field_types_by_type_name(type_name)
+        new_references = {}
         for ref in references:
-            new_references[ref['type_name']]=ref["required"]
+            new_references[ref['type_name']] = ref["required"]
         return new_references
 
     def get_reference_type_names(self):
@@ -54,7 +57,8 @@ class ReferenceService():
         rr.delete_reference(ref_key)
 
     def get_all_references(self):
-        references = organise_references_for_output(rr.get_all_references_with_entries())
+        references = organise_references_for_output(
+            rr.get_all_references_with_entries())
         return references
 
     def get_reference_entries(self):
@@ -66,5 +70,6 @@ class ReferenceService():
     def check_if_empty(self, entry):
         if entry == "":
             raise UserInputError("Field required!")
+
 
 reference_service = ReferenceService()
